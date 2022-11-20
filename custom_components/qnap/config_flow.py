adf61394ad7b7c8ad33fd358c75006c5a06d001b
disforw <github@benzona.net>
2022-11-20 16:12:58 -0500

    Update config_flow.py

--- custom_components/qnap/config_flow.py
+++ custom_components/qnap/config_flow.py
@@ -6,6 +6,7 @@ from requests.exceptions import ConnectTimeout
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.components import dhcp
 from homeassistant.components.persistent_notification import create as notify_create
 from homeassistant.const import (
     CONF_HOST,
@@ -36,6 +37,9 @@ _LOGGER = logging.getLogger(__name__)
 class QnapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Qnap configuration flow."""
 
+    _discovered_mac: str | None = None
+    _discovered_url: str | None = None
+    
     VERSION = 1
 
     def __init__(self):
@@ -72,7 +76,7 @@ class QnapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "unknown"
             else:
                 unique_id = stats["system"]["serial_number"]
-                await self.async_set_unique_id(unique_id)
+                await self.async_set_unique_id(unique_id, raise_on_progress=False)
                 self._abort_if_unique_id_configured()
                 title = stats["system"]["name"].capitalize()
                 if self.is_imported:
@@ -84,4 +88,62 @@ class QnapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+    
+    async def async_step_dhcp(self, discovery_info: dhcp.DhcpServiceInfo) -> FlowResult:
+        """Handle DHCP discovery."""
+        self._discovered_url = f"http://{discovery_info.ip}"
+        self._discovered_mac = discovery_info.macaddress
+
+        _LOGGER.debug("DHCP discovery detected QNAP: %s", self._discovered_mac)
+
+        mac = format_mac(self._discovered_mac)
+        options = ConnectionOptions(self._discovered_url, "", "")
+        qsw = QnapQswApi(aiohttp_client.async_get_clientsession(self.hass), options)
+
+        try:
+            await qsw.get_live()
+        except QswError as err:
+            raise AbortFlow("cannot_connect") from err
+
+        await self.async_set_unique_id(format_mac(mac))
+        self._abort_if_unique_id_configured()
+
+        return await self.async_step_discovered_connection()
+
+    async def async_step_discovered_connection(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm discovery."""
+        errors = {}
+        assert self._discovered_url is not None
+
+        if user_input is not None:
+            username = user_input[CONF_USERNAME]
+            password = user_input[CONF_PASSWORD]
+
+            qsw = QnapQswApi(
+                aiohttp_client.async_get_clientsession(self.hass),
+                ConnectionOptions(self._discovered_url, username, password),
+            )
+
+            try:
+                system_board = await qsw.validate()
+            except LoginError:
+                errors[CONF_PASSWORD] = "invalid_auth"
+            except QswError:
+                errors[CONF_URL] = "cannot_connect"
+            else:
+                title = f"QNAP {system_board.get_product()} {self._discovered_mac}"
+                return self.async_create_entry(title=title, data=user_input)
+
+        return self.async_show_form(
+            step_id="discovered_connection",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                }
+            ),
+            errors=errors,
         )

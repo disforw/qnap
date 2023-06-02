0a04626277b3232c0cebbe141a39d51f20b1e2f9
disforw <github@benzona.net>
2023-06-02 01:58:09 -0400

    Update sensor.py

--- custom_components/qnap/sensor.py
+++ custom_components/qnap/sensor.py
@@ -1,6 +1,7 @@
 """Support for QNAP NAS Sensors."""
 import logging
 
+from homeassistant import config_entries
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -44,7 +45,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigType,
+    config_entry: config_entries.ConfigEntry,
     async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up entry."""

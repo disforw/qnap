02d2a3a6a5a5a7846494823ba3cabefb8803617f
disforw <github@benzona.net>
2023-06-10 08:10:43 -0400

    Update coordinator.py

--- custom_components/qnap/coordinator.py
+++ custom_components/qnap/coordinator.py
@@ -1,49 +1,38 @@
-"""Data coordinator for the dwd_weather_warnings integration."""
+"""Data coordinator for the qnap integration."""
 
 from __future__ import annotations
-
-from dwdwfsapi import DwdWeatherWarningsAPI
+from qnapstats import QNAPStats
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DEFAULT_SCAN_INTERVAL, DOMAIN, LOGGER
+from .const import DOMAIN
+UPDATE_INTERVAL = timedelta(minutes=1)
+
+_LOGGER = logging.getLogger(__name__)
 
 
-class DwdWeatherWarningsCoordinator(DataUpdateCoordinator[None]):
-    """Custom coordinator for the dwd_weather_warnings integration."""
+class QnapCoordinator(DataUpdateCoordinator[None]):
+    """Custom coordinator for the qnap integration."""
 
-    def __init__(self, hass: HomeAssistant, api: DwdWeatherWarningsAPI) -> None:
-        """Initialize the dwd_weather_warnings coordinator."""
+    def __init__(self, hass: HomeAssistant, api: QNAPStats) -> None:
+        """Initialize the qnap coordinator."""
         super().__init__(
-            hass, LOGGER, name=DOMAIN, update_interval=DEFAULT_SCAN_INTERVAL
+            hass, LOGGER, name=DOMAIN, update_interval=UPDATE_INTERVAL
         )
 
         self.api = api
 
     async def _async_update_data(self) -> None:
-        """Get the latest data from the DWD Weather Warnings API."""
-        await self.hass.async_add_executor_job(self.api.update)
------
-        
-async def async_update_data():
+        """Get the latest data from the Qnap API."""
         datas = {}
-        datas["system_stats"] = await hass.async_add_executor_job(api.get_system_stats)
+        datas["system_stats"] = await hass.async_add_executor_job(self.api.get_system_stats)
         datas["system_health"] = await hass.async_add_executor_job(
-            api.get_system_health
+            self.api.get_system_health
         )
         datas["smart_drive_health"] = await hass.async_add_executor_job(
-            api.get_smart_disk_health
+            self.api.get_smart_disk_health
         )
-        datas["volumes"] = await hass.async_add_executor_job(api.get_volumes)
-        datas["bandwidth"] = await hass.async_add_executor_job(api.get_bandwidth)
+        datas["volumes"] = await hass.async_add_executor_job(self.api.get_volumes)
+        datas["bandwidth"] = await hass.async_add_executor_job(self.api.get_bandwidth)
         return datas
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="sensor",
-        update_method=async_update_data,
-        update_interval=UPDATE_INTERVAL,
-    )
-

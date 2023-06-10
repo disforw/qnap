58ff533e2950eff81139390b26c02ead8e50b0fa
disforw <github@benzona.net>
2023-06-10 08:53:24 -0400

    Fix hass

--- custom_components/qnap/coordinator.py
+++ custom_components/qnap/coordinator.py
@@ -1,6 +1,8 @@
 """Data coordinator for the qnap integration."""
-
 from __future__ import annotations
+
+import logging
+from datetime import timedelta
 from qnapstats import QNAPStats
 
 from homeassistant.core import HomeAssistant
@@ -26,13 +28,13 @@ class QnapCoordinator(DataUpdateCoordinator[None]):
     async def _async_update_data(self) -> None:
         """Get the latest data from the Qnap API."""
         datas = {}
-        datas["system_stats"] = await hass.async_add_executor_job(self.api.get_system_stats)
-        datas["system_health"] = await hass.async_add_executor_job(
+        datas["system_stats"] = await self.hass.async_add_executor_job(self.api.get_system_stats)
+        datas["system_health"] = await self.hass.async_add_executor_job(
             self.api.get_system_health
         )
-        datas["smart_drive_health"] = await hass.async_add_executor_job(
+        datas["smart_drive_health"] = await self.hass.async_add_executor_job(
             self.api.get_smart_disk_health
         )
-        datas["volumes"] = await hass.async_add_executor_job(self.api.get_volumes)
-        datas["bandwidth"] = await hass.async_add_executor_job(self.api.get_bandwidth)
+        datas["volumes"] = await self.hass.async_add_executor_job(self.api.get_volumes)
+        datas["bandwidth"] = await self.hass.async_add_executor_job(self.api.get_bandwidth)
         return datas

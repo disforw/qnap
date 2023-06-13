401e38747e6f7e6eef17660cd3401f5a7395feca
disforw <github@benzona.net>
2023-06-13 19:11:32 -0400

    Update __init__.py

--- custom_components/qnap/__init__.py
+++ custom_components/qnap/__init__.py
@@ -31,7 +31,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     if not coordinator.last_update_success:
         raise PlatformNotReady
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
 

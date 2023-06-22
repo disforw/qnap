dbe74c349509ab444c7ddd10de11823afd7ed551
disforw <github@benzona.net>
2023-06-22 09:26:39 -0400

    Update const.py

--- custom_components/qnap/const.py
+++ custom_components/qnap/const.py
@@ -1,12 +1,12 @@
 """The Qnap constants."""
 
-DEFAULT_NAME = "QNAP"
+CONF_DRIVES = "drives"
+CONF_NICS = "nics"
+CONF_VOLUMES = "volumes"
+
 DEFAULT_PORT = 8080
 DEFAULT_TIMEOUT = 5
 DEFAULT_SSL = False
 DEFAULT_VERIFY_SSL = True
 
 DOMAIN = "qnap"
-NOTIFICATION_ID = "qnap_notification"
-NOTIFICATION_TITLE = "QNAP Sensor Setup"
-VOLUME_NAME = "volume"
